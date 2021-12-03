2e0933a387342b0920de8d38d7f0f26b13ef8fd7
Jacob Reimers <jacob@reimers.dk>
2021-12-03 14:53:28 +0100

    Adding manifests.

--- openmedstack.sample/src/main/kotlin/org/openmedstack/sample/Program.kt
+++ openmedstack.sample/src/main/kotlin/org/openmedstack/sample/Program.kt
@@ -3,14 +3,19 @@ package org.openmedstack.sample
 import org.openmedstack.Chassis
 import org.openmedstack.DeploymentConfiguration
 import org.openmedstack.domain.guice.EventStoreModule
+import org.openmedstack.messaging.guice.DomainModule
 import org.openmedstack.messaging.guice.InMemoryMessagingModule
+import java.net.URI
 
-class Program {
-    fun main(vararg args: String?) {
-        val configuration = DeploymentConfiguration()
-        configuration.name = "sample"
-        val chassis = Chassis.from(configuration)
-            .withServiceBuilder { _,_-> DefaultService(EventStoreModule(), InMemoryMessagingModule()) }
-        chassis.start()
-    }
+fun main(args: Array<String>) {
+    println("Starting")
+    val configuration = DeploymentConfiguration()
+    configuration.name = "sample"
+    configuration.serviceBus = URI.create("http://localhost")
+    val chassis = Chassis.from(configuration)
+        .withServiceBuilder { _, _ -> DefaultService(EventStoreModule(), InMemoryMessagingModule(), DomainModule()) }
+    chassis.start()
+    println("Started")
+    chassis.close()
+    println("Finished")
 }
